68f9620d4c56e014bb419eb31fab7b8d317a31d3
Ghasem Kiani <ghasemkiani@gmail.com>
2025-05-22 19:42:51 +0330

    update

--- document.js
+++ document.js
@@ -1,39 +1,39 @@
 //	@ghasemkiani/wjsdom/document
 
-import {JSDOM} from "jsdom";
+import { JSDOM } from "jsdom";
 
-import {cutil} from "@ghasemkiani/base";
-import {WDocument as WDocumentBase} from "@ghasemkiani/wdom";
+import { cutil } from "@ghasemkiani/base";
+import { WDocument as WDocumentBase } from "@ghasemkiani/wdom";
 
 class WDocument extends WDocumentBase {
-	static {
-		cutil.extend(this.prototype, {
-			defaultMime: "application/xml",
-			preamble: "<!DOCTYPE html>",
-		});
-	}
-	getWindow() {
-		let contentType = this.mime;
-		let text = `<!DOCTYPE html><html xmlns="http://www.w3.org/1999/xhtml"></html>`;
-		let {window} = new JSDOM(text, {contentType});
-		return window;
-	}
-	fromString(string) {
-		this._window = null;
-		this._document = null;
-		this._root = null;
-		let contentType = this.mime;
-		let text = string;
-		let {window} = new JSDOM(text, {contentType});
-		this.window = window;
-	}
-	toString() {
-		let text = this.preamble;
-		if(this.root) {
-			text += this.root.string;
-		}
-		return text;
-	}
+  static {
+    cutil.extend(this.prototype, {
+      defaultMime: "application/xml",
+      preamble: "<!DOCTYPE html>",
+    });
+  }
+  getWindow() {
+    let contentType = this.mime;
+    let text = `<!DOCTYPE html><html xmlns="http://www.w3.org/1999/xhtml"></html>`;
+    let { window } = new JSDOM(text, { contentType });
+    return window;
+  }
+  fromString(string) {
+    this._window = null;
+    this._document = null;
+    this._root = null;
+    let contentType = this.mime;
+    let text = string;
+    let { window } = new JSDOM(text, { contentType });
+    this.window = window;
+  }
+  toString() {
+    let text = this.preamble;
+    if (this.root) {
+      text += this.root.string;
+    }
+    return text;
+  }
 }
 
-export {WDocument};
+export { WDocument };
